3811e2dc6cd145cbad873c4a082800d2810cf55e
quooctrangg <nguyenquoctrang188@gmail.com>
2023-09-06 08:49:21 +0700

    change port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import dotenv from 'dotenv'
+dotenv.config()
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		port: process.env.PORT,
+		strictPort: false,
 	}
 };
 
