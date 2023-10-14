71bb26d72ea46ea9e459c6dc72a98974e9a5f3a8
quooctrangg <nguyenquoctrang188@gmail.com>
2023-10-14 22:28:58 +0700

    add logout function

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,10 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto)
+    }
+
+    @Post('logout')
+    logout(@Res() res: Response) {
+
     }
 }
