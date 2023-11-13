ee8a96cfc09d4bb1e0484811e333a6754ef68f6b
quooctrangg <nguyenquoctrang188@gmail.com>
2023-11-13 19:31:46 +0700

    add create practice

--- server/src/practice/practice.controller.ts
+++ server/src/practice/practice.controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { PracticeService } from './practice.service';
+import { GetAccount, Roles } from '../auth/decorator';
+import { Account } from '@prisma/client';
+import { CreatePraticeDto } from './dto';
+import { MyJWTGuard, RolesGuard } from '../auth/guard';
+import { ACCOUNT_TYPES } from '../global';
 
 
 @Controller('practice')
 export class PracticeController {
     constructor(private practiceService: PracticeService) { }
 
+    @Post()
+    @UseGuards(MyJWTGuard, RolesGuard)
+    @Roles(ACCOUNT_TYPES.ADMIN, ACCOUNT_TYPES.USER)
+    createPractice(@GetAccount() account: Account, createPracticeDto: CreatePraticeDto) {
+        return this.practiceService.createPractice(account.userId, createPracticeDto)
+    }
 }
--- server/src/practice/practice.service.ts
+++ server/src/practice/practice.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreatePraticeDto } from './dto';
+import { ResponseData } from '../global';
+import { Practice } from '@prisma/client';
 
 @Injectable()
 export class PracticeService {
     constructor(private prismaService: PrismaService) { }
 
+    async createPractice(userId: number, createPracticeDto: CreatePraticeDto) {
+        try {
+            const data = await this.prismaService.practice.create({
+                data: {
+                    userId: userId,
+                    levelId: createPracticeDto.levelId,
+                    typeId: createPracticeDto.typeId,
+                    specializationId: createPracticeDto.specializationId,
+                    numOfSentence: createPracticeDto.numOfSentence,
+                    nRight: createPracticeDto.nRight,
+                    nWrong: createPracticeDto.nWrong,
+                    nRightConsecitive: createPracticeDto.nRightConsecitive,
+                    Words: {
+                        connect: createPracticeDto.words.map((id) => ({ id }))
+                    }
+                }
+            })
+            return new ResponseData<Practice>(data, 200, 'Lưu thành công kết quả bài thi')
+        } catch (error) {
+            return new ResponseData<string>(null, 500, 'Lỗi dịch vụ, thử lại sau')
+        }
+    }
 }
