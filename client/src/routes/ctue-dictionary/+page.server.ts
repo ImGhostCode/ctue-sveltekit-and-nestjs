ff7744c96418bdfc1e52414815ba8b3cb2c7408c
quooctrangg <nguyenquoctrang188@gmail.com>
2023-11-16 23:54:19 +0700

    add get all words

---
+++ client/src/routes/ctue-dictionary/+page.server.ts
@@ -0,0 +1,22 @@
+import type { PageServerLoad } from './$types';
+import * as db from '$lib/server/database';
+
+
+type Types = { id: number, name: string, isWord: boolean }
+type Levels = { id: number, name: string }
+type Specializations = { id: number, name: string }
+type Topics = { id: number, name: string, isWord: boolean, selected: boolean, image: string }
+
+export const load: PageServerLoad = async ({ cookies }) => {
+    const types: Types[] = await db.getTypes(true)
+    const levels: Levels[] = await db.getLevels()
+    const specializations: Specializations[] = await db.getSpecializations()
+    let topics: Topics[] = await db.getTopics(true)
+
+    topics = topics.map(topic => {
+        return { ...topic, selected: false }
+    })
+    return {
+        types, levels, specializations, topics
+    }
+};
