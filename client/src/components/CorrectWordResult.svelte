e269ba2bee804d1656b98a6c0b67ba1f16e3fb0f
imghostcode <imghostcode>
2023-11-17 22:14:16 +0700

    show questions in currect word page

---
+++ client/src/components/CorrectWordResult.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let onReplay;
+	export let nRight;
+	export let nWrong;
+	export let nRightConsecutive;
+</script>
+
+<h1>result</h1>
---
+++ client/src/constants/practice.ts
@@ -0,0 +1,5 @@
+export const CONFUSING_LIST = 20
+export const DELAY_TIME = 1500
+export const DELAY_ANSWER = 3000
+
+
